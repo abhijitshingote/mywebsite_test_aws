b98bf57f2309fbe8f1d73a7db059faa0678a9a89
Abhijit Shingote <abhijitshingote@Abhijits-MacBook-Pro.local>
2019-08-25 14:25:17 -0400

    temperature

--- someapp/views.py
+++ someapp/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 
 # Create your views here.
 def index(request):
-	return HttpResponse('Hello R A D H I K A !')
+	# return HttpResponse('Hello R A D H I K A !')
+	# weatherurl='http://www.metaweather.com/api/location/44418/'
+	# temperature=requests.get(weatherurl).json()['consolidated_weather'][0]['the_temp']
+	# city=requests.get(weatherurl).json()['title']
+	tempdict={}
+	for cityid in ['44418','12586539','2378426','2459115']:
+		weatherurl='http://www.metaweather.com/api/location/' + cityid + '/'
+		temperature=requests.get(weatherurl).json()['consolidated_weather'][0]['the_temp']
+		city=requests.get(weatherurl).json()['title']
+		tempdict[city]=temperature
+	# context={'city':city,'temperature':temperature}
+	return render(request,'index.html',context={'tempdict':tempdict})
+
